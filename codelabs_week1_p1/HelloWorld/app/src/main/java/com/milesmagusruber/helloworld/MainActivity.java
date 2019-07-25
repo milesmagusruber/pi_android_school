a629437e3051c283004c9d0d5260583b92fc3c06
miles.magus.ruber <miles.magus.ruber@gmail.com>
2019-07-25 22:27:41 +0300

    Made first part of android codelabs for week1

---
+++ codelabs_week1_p1/HelloWorld/app/src/main/java/com/milesmagusruber/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.milesmagusruber.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        //Exception code challenge
+        try{
+            throw new Exception("Checking exception");
+        }catch(Exception e){
+            Log.e("MainActivity",e.getMessage());
+        }
+
+    }
+}
