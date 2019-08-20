b862f31d4de660e45aa45ede58f009c32794e669
miles.magus.ruber <miles.magus.ruber@gmail.com>
2019-08-20 08:47:16 +0300

    new empty activity created

---
+++ SecretServiceFlickrSearch/app/src/main/java/com/milesmagusruber/secretserviceflickrsearch/activities/GoogleMapsSearchActivity.java
@@ -0,0 +1,16 @@
+package com.milesmagusruber.secretserviceflickrsearch.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.milesmagusruber.secretserviceflickrsearch.R;
+
+public class GoogleMapsSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps_search);
+    }
+}
