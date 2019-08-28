4a4b4e075110793c5ecc8d552a83d262b4eb29c9
miles.magus.ruber <miles.magus.ruber@gmail.com>
2019-08-28 21:23:13 +0300

    Empty GalleryActivity is created

---
+++ SecretServiceFlickrSearch/app/src/main/java/com/milesmagusruber/secretserviceflickrsearch/activities/GalleryActivity.java
@@ -0,0 +1,16 @@
+package com.milesmagusruber.secretserviceflickrsearch.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.milesmagusruber.secretserviceflickrsearch.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
