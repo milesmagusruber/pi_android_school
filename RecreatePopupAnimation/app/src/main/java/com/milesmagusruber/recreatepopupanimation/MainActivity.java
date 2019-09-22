f78a6b463758dc758b0a45ca49b03de17a00cedd
miles.magus.ruber <miles.magus.ruber@gmail.com>
2019-09-22 23:04:54 +0300

    Beginning of popup window creation

--- RecreatePopupAnimation/app/src/main/java/com/milesmagusruber/recreatepopupanimation/MainActivity.java
+++ RecreatePopupAnimation/app/src/main/java/com/milesmagusruber/recreatepopupanimation/MainActivity.java
@@ -1,7 +1,10 @@
 package com.milesmagusruber.recreatepopupanimation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View customPopup = layoutInflater.inflate(R.layout.custom_popup, null);
+
+        // Create a AlertDialog Builder.
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(customPopup);
+        alertDialogBuilder.show();
     }
 }
