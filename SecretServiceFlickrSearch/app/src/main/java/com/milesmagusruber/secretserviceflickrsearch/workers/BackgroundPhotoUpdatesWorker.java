d19ea8bf911a3d33dcad6d2826c3f629ee1ae71c
miles.magus.ruber <miles.magus.ruber@gmail.com>
2019-09-28 00:33:48 +0300

    In process of worker improvement

--- SecretServiceFlickrSearch/app/src/main/java/com/milesmagusruber/secretserviceflickrsearch/workers/BackgroundPhotoUpdatesWorker.java
+++ SecretServiceFlickrSearch/app/src/main/java/com/milesmagusruber/secretserviceflickrsearch/workers/BackgroundPhotoUpdatesWorker.java
@@ -1,10 +1,14 @@
 package com.milesmagusruber.secretserviceflickrsearch.workers;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -14,12 +18,26 @@ import androidx.work.WorkerParameters;
 import com.milesmagusruber.secretserviceflickrsearch.R;
 import com.milesmagusruber.secretserviceflickrsearch.db.SSFSDatabase;
 import com.milesmagusruber.secretserviceflickrsearch.db.entities.RequestedPhoto;
+import com.milesmagusruber.secretserviceflickrsearch.network.NetworkHelper;
+import com.milesmagusruber.secretserviceflickrsearch.network.model.FlickrResponse;
+import com.milesmagusruber.secretserviceflickrsearch.network.model.Photo;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class BackgroundPhotoUpdatesWorker extends Worker {
 
+    //Working with db
     private SSFSDatabase db;
+
+    //Working with Flickr via network
+    private NetworkHelper networkHelper;
+    private Call<FlickrResponse> call;
+    private boolean downloadSuccess;
 
     public BackgroundPhotoUpdatesWorker(
             @NonNull Context appContext,
@@ -36,9 +54,42 @@ public class BackgroundPhotoUpdatesWorker extends Worker {
         String searchRequest = "Cat";
         try {
             //TODO: Downloading Flickr Photos
+            call = networkHelper.getSearchTextQueryPhotos(searchRequest,1);
+            call.enqueue(new Callback<FlickrResponse>() {
+                @Override
+                public void onResponse(Call<FlickrResponse> call, Response<FlickrResponse> response) {
+                    FlickrResponse flickrResponse = response.body();
+
+                    List<Photo> photos = null;
+                    //If Response is not null making a result list of photos
+                    if (flickrResponse != null) {
+
+                        photos = flickrResponse.getPhotos().getPhoto();
+
+                    }
+                    //If photos not null show them
+                    if (photos != null && !photos.isEmpty()) {
+                        //do one thing
+                    } else {
+                        //do another
+                    }
+
+                }
+
+                //If we fail then set an error string to textview
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onFailure(Call<FlickrResponse> call, Throwable t) {
+                    Log.e(TAG, "onFailure: Error");
+                    Log.e(TAG, t.toString());
+                    downloadSuccess = false;
+                }
+            });
+
+            //If download wasn't successful return failure
+            if(!downloadSuccess) return Result.failure();
 
             ArrayList<RequestedPhoto> requestedPhotos = new ArrayList<>();
-
 
             //Saving photos to database
 
