3681fce387faac17b431c2e82272fda87f0a8e7c
miles.magus.ruber <miles.magus.ruber@gmail.com>
2019-09-28 00:18:55 +0300

    Working with db in worker

--- SecretServiceFlickrSearch/app/src/main/java/com/milesmagusruber/secretserviceflickrsearch/workers/BackgroundPhotoUpdatesWorker.java
+++ SecretServiceFlickrSearch/app/src/main/java/com/milesmagusruber/secretserviceflickrsearch/workers/BackgroundPhotoUpdatesWorker.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -11,8 +12,14 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import com.milesmagusruber.secretserviceflickrsearch.R;
+import com.milesmagusruber.secretserviceflickrsearch.db.SSFSDatabase;
+import com.milesmagusruber.secretserviceflickrsearch.db.entities.RequestedPhoto;
+
+import java.util.ArrayList;
 
 public class BackgroundPhotoUpdatesWorker extends Worker {
+
+    private SSFSDatabase db;
 
     public BackgroundPhotoUpdatesWorker(
             @NonNull Context appContext,
@@ -20,13 +27,39 @@ public class BackgroundPhotoUpdatesWorker extends Worker {
         super(appContext, workerParams);
     }
 
+    private static final String TAG = BackgroundPhotoUpdatesWorker.class.getSimpleName();
+
     @NonNull
     @Override
-    public Result doWork(){
-        showNotification("WorkManager", "Test message");
-        return Result.success();
+    public Result doWork() {
+        Context applicationContext = getApplicationContext();
+        String searchRequest = "Cat";
+        try {
+            //TODO: Downloading Flickr Photos
+
+            ArrayList<RequestedPhoto> requestedPhotos = new ArrayList<>();
+
+
+            //Saving photos to database
+
+            //getting database
+            db = db.getInstance(applicationContext);
+            //deleting all previous requested photos
+            db.requestedPhotoDao().deleteAll();
+            //inserting all new requested photos
+            db.requestedPhotoDao().insertAll(requestedPhotos);
+
+            //Showing Notification about success download
+            showNotification("WorkManager", "Test message");
+            return Result.success();
+        } catch (Throwable throwable) {
+            Log.e(TAG, "Error in background photo updates", throwable);
+            return Result.failure();
+        }
     }
 
+
+    //Showing Notifications
     private void showNotification(String task, String desc) {
 
         NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
